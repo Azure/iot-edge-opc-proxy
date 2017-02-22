1323f89169f7bba0b3e8cbd2d9b47f90d1dc5705
Marc Schier <marcschier@hotmail.com>
2017-02-22 12:45:12 +0100

    Fix warning

--- inc/os_linux.h
+++ inc/os_linux.h
@@ -7,15 +7,36 @@
 //
 // Linux OS api used in PAL implementation
 //
+
+// Include default apis ...
+#if !defined(_DEFAULT_SOURCE)
 #define _DEFAULT_SOURCE 1
 #define _BSD_SOURCE 1
 #define _SVID_SOURCE 1
-#define _XOPEN_SOURCE 600
-#define _GNU_SOURCE 1
-#define _FORTIFY_SOURCE 1
-#define _ISOC99_SOURCE 1
-#include <features.h>
+#endif
 
+// ... plus xopen and posix apis 
+#if !defined(_XOPEN_SOURCE) || (_XOPEN_SOURCE < 600)
+#undef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 600
+#endif
+
+// ... and any linux specific ones
+#if !defined(_GNU_SOURCE)
+#define _GNU_SOURCE 1
+#endif
+
+// Ensure safe string manipulation ...
+#if !defined(_FORTIFY_SOURCE)
+#define _FORTIFY_SOURCE 1
+#endif
+
+// ... and C'99 compliant source
+#if !defined(_ISOC99_SOURCE)
+#define _ISOC99_SOURCE 1
+#endif
+
+#include <features.h>
 #include "os_posix.h"
 
 #endif // _os_linux_h_
