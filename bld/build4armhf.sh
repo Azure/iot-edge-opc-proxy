c4d7d1c96c3f54517149b9cb3f301ece8b596d6a
Juergen Kosel <juergen.kosel@softing.com>
2017-02-24 11:49:38 +0100

    Disable unit test while cross compiling
    The cross compiled unit tests can not be executed on the building machine.
    So cross compiling implicitly means skip unit tests!
    
    Signed-off-by: Juergen Kosel <juergen.kosel@softing.com>

--- bld/build4armhf.sh
+++ bld/build4armhf.sh
@@ -10,5 +10,5 @@ cmake -DCMAKE_TOOLCHAIN_FILE=${repo_root}/bld/cmake_toolchain_file_armhf_oriole 
       -DOPENSSL_ROOT_DIR=$ARMHF_SYSROOT_DIR/usr \
       -DOPENSSL_INCLUDE_DIR=$ARMHF_SYSROOT_DIR/usr/include \
       -DOPENSSL_LIBRARIES="-L$ARMHF_SYSROOT_DIR/usr/lib/arm-linux-gnueabihf -lssl -lcrypto" \
-      -DCMAKE_BUILD_TYPE=Debug -Dskip_unittests:BOOL=ON \
+      -DCMAKE_BUILD_TYPE=Debug \
       -Duse_zlog:BOOL=OFF "$repo_root"
