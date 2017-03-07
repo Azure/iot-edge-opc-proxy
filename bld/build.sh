c3fd92db04ddce5e2ef074246ba30bb0cf9f63bd
Juergen Kosel <juergen.kosel@softing.com>
2017-03-07 14:23:59 +0100

    Linux build improvement (#19)
    * Renamed cross compile cmake file
    
    - Use a more meaning-full name for the cmake cross compile configuration
      for cross compile host linux-armhf.
    
    Signed-off-by: Juergen Kosel <juergen.kosel@softing.com>
    
    * Enable IPv6 support in libwebsockets for Linux builds
    
    Signed-off-by: Juergen Kosel <juergen.kosel@softing.com>
    
    * Use getopt to parse options of build.sh script
    
    Replace the hand written parsing of the options and arguments of build.sh
    with getopt (see man 1 getopt).
    
    Signed-off-by: Juergen Kosel <juergen.kosel@softing.com>
    
    * Add options to let the user decide how many make jobs should be started
    
    The number of make jobs which a suitable for a PC depend on the number of
    cores, the memory, current load,..
    So we let the user decide with the new added option -j <value> how
    many jobs should be started in parallel.
    
    Signed-off-by: Juergen Kosel <juergen.kosel@softing.com>

--- bld/build.sh
+++ bld/build.sh
@@ -2,21 +2,13 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-set -e
-
 current_root=$(pwd)
 repo_root=$(cd "$(dirname "$0")/.." && pwd)
 
 skip_unittests=OFF
 skip_dotnet=0
 use_zlog=OFF
-if [ `nproc` -gt 1 ]
-then
-    # Use up to 2 parrallel jobs, if the machine has more than one core
-    MAKE_PARALLEL_JOBS=2
-else
-    MAKE_PARALLEL_JOBS=1
-fi
+MAKE_PARALLEL_JOBS=1
 
 build_root="${repo_root}"/build
 build_clean=0
@@ -29,16 +21,19 @@ usage ()
 {
     echo "build.sh [options]"
     echo "options"
-    echo "    --os <value>			   [] Os to build on (needs Docker installed)."
-    echo " -c --clean                  Build clean (Removes previous build output)"
-    echo " -C --config <value>         [Debug] Build configuration (e.g. Debug, Release)"
-    echo " -o --build-root <value>     [/build] Directory in which to place all files during build"
-    echo "    --use-zlog               Use zlog as logging framework instead of xlogging"
-    echo " 	  --skip-unittests         Skips building and executing unit tests"
-    echo "	  --skip-dotnet            Do not build dotnet core API and packages"
-    echo "    --pack-only              Only creates packages. (Cannot be combined with --clean)"
-    echo " -n --nuget-folder <value>   [/build] Folder to use when outputting nuget packages."
-    echo " -x --xtrace                 print a trace of each command"
+    echo "    --os <value>                [] Os to build on (needs Docker installed)."
+    echo " -c --clean                     Build clean (Removes previous build output)"
+    echo " -C --config <value>            [Debug] Build configuration (e.g. Debug, Release)"
+    echo " -o --build-root <value>        [/build] Directory in which to place all files during build"
+    echo "    --use-zlog                  Use zlog as logging framework instead of xlogging"
+    echo "    --skip-unittests            Skips building and executing unit tests"
+    echo "    --skip-dotnet               Do not build dotnet core API and packages"
+    echo "    --pack-only                 Only creates packages. (Cannot be combined with --clean)"
+    echo " -n --nuget-folder <value>      [/build] Folder to use when outputting nuget packages."
+    echo " -x --xtrace                    print a trace of each command"
+    echo " -j <value> | --jobs <value>    Number of parallel make jobs, see description of make -j"
+    echo "                                Unit test need a lot of memory. So --skip-unittests should be set,"
+    echo '                                if you want to use all cores with -j`nproc`'
     exit 1
 }
 
@@ -47,50 +42,69 @@ usage ()
 # -----------------------------------------------------------------------------
 process_args ()
 {
-    save_next_arg=0
-    for arg in $*; do
-		  if [ $save_next_arg == 1 ]; then
-			build_root="$arg"
-			save_next_arg=0
-		elif [ $save_next_arg == 2 ]; then
-			build_configs+=("$arg")
-			save_next_arg=0
-		elif [ $save_next_arg == 3 ]; then
-			build_nuget_output="$arg"
-			save_next_arg=0
-		elif [ $save_next_arg == 4 ]; then
-			build_os=="$arg"
-			save_next_arg=0
-		else
-			case "$arg" in
-				-x | --xtrace)
-					set -x;;
-				-o | --build-root)
-					save_next_arg=1;;
-				-C | --config)
-					save_next_arg=2;;
-				-c | --clean)
-					build_clean=1;;
-				--use-zlog)
-					use_zlog=ON;;
-				--use-openssl)
-					;;
-				--use-libwebsockets)
-					;;
-				--pack-only)
-					build_pack_only=1;;
-				--skip-unittests)
-					skip_unittests=ON;;
-				--skip-dotnet)
-					skip_dotnet=1;;
-				--os)
-					save_next_arg=4;;
-				-n | --nuget-folder)
-					save_next_arg=3;;
-				*)
-					usage;;
-			esac
-		fi
+    # Note that we use `"$@"' to let each command-line parameter expand to a
+    # separate word. The quotes around `$@' are essential!
+    # We need TEMP as the `eval set --' would nuke the return value of getopt.
+    TEMP=`getopt -o xo:C:cn:j: \
+          -l xtrace,build-root:,config:,clean,use-zlog,use-openssl,use-libwebsockets,pack-only,skip-unittests,skip-dotnet,os:,nuget-folder:,jobs: \
+         -- "$@"`
+
+    if [ $? != 0 ]
+    then
+        echo "Failed to parse options"
+        usage
+        exit 1
+    fi
+
+    # Note the quotes around `$TEMP': they are essential!
+    eval set -- "$TEMP"
+
+    while true
+    do
+	case "$1" in
+	    -x | --xtrace)
+		set -x
+                shift ;;
+	    -o | --build-root)
+		build_root="$2"
+                shift 2 ;;
+	    -C | --config)
+                build_configs+=("$2")
+                shift 2 ;;
+	    -c | --clean)
+		build_clean=1
+                shift ;;
+	    --use-zlog)
+		use_zlog=ON
+                shift ;;
+	    --use-openssl)
+                shift ;;
+	    --use-libwebsockets)
+                shift ;;
+	    --pack-only)
+		build_pack_only=1
+                shift ;;
+	    --skip-unittests)
+		skip_unittests=ON
+                shift ;;
+	    --skip-dotnet)
+		skip_dotnet=1
+                shift ;;
+	    --os)
+		build_os="$2"
+                shift 2 ;;
+	    -n | --nuget-folder)
+		build_nuget_output="$2"
+                shift 2 ;;
+            -j | --jobs)
+                MAKE_PARALLEL_JOBS=$2
+                shift 2 ;;
+	    --) shift
+                break ;; # Terminate the while loop at the last option
+	    *)
+		usage
+                shift ;
+	esac
     done
 }
 
@@ -107,8 +121,9 @@ native_build()
 			cd "${build_root}/cmake/${c}" > /dev/null
 
 			cmake -DCMAKE_BUILD_TYPE=$c -Dskip_unittests:BOOL=$skip_unittests \
-					-Duse_zlog:BOOL=$use_zlog "$repo_root" || \
-				return 1 
+			      -Duse_zlog:BOOL=$use_zlog "$repo_root" \
+                              -DLWS_IPV6:BOOL=ON || \
+			    return 1
                         # Start as much parallel jobs as requested by the user.
                         # Until the load average equals the number of cores.
                         # Be verbose if something goes wrong
@@ -207,6 +222,9 @@ main()
 }
 
 process_args $*
+
+set -e
+
 main
 echo "$?" 
 cd "$current_root" > /dev/null
--- bld/build4armhf.sh
+++ bld/build4armhf.sh
@@ -6,9 +6,10 @@
 repo_root=$(cd "$(dirname "$0")/.." && pwd)
 
 # Also assuming that the current working directory is the desired directory for the build
-cmake -DCMAKE_TOOLCHAIN_FILE=${repo_root}/bld/cmake_toolchain_file_armhf_oriole \
+cmake -DCMAKE_TOOLCHAIN_FILE=${repo_root}/bld/cmake_toolchain_file_armhf_linux \
       -DOPENSSL_ROOT_DIR=$ARMHF_SYSROOT_DIR/usr \
       -DOPENSSL_INCLUDE_DIR=$ARMHF_SYSROOT_DIR/usr/include \
       -DOPENSSL_LIBRARIES="-L$ARMHF_SYSROOT_DIR/usr/lib/arm-linux-gnueabihf -lssl -lcrypto" \
       -DCMAKE_BUILD_TYPE=Debug \
-      -Duse_zlog:BOOL=OFF "$repo_root"
+      -Duse_zlog:BOOL=OFF "$repo_root" \
+      -DLWS_IPV6:BOOL=ON
