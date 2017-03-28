a92f9c38b1773962b67cfd1879f4ef2ac565253a
Marc Schier <marcschier@hotmail.com>
2017-03-28 08:23:42 +0200

    Update build.sh

--- bld/build.sh
+++ bld/build.sh
@@ -26,10 +26,10 @@ usage ()
     echo "    --os <value>                [] Os to build on (needs Docker installed)."
     echo " -c --clean                     Build clean (Removes previous build output)."
     echo " -C --config <value>            [Debug] Build configuration (e.g. Debug, Release)."
-	echo " -T --toolset <value>           An optional toolset to use."
+    echo " -T --toolset <value>           An optional toolset to use."
     echo " -o --build-root <value>        [/build] Directory in which to place all files during build."
     echo "    --use-zlog                  Use zlog as logging framework instead of xlogging."
-	echo "    --with-memcheck             Compile in memory checks."
+    echo "    --with-memcheck             Compile in memory checks."
     echo "    --skip-unittests            Skips building and executing unit tests."
     echo "    --skip-dotnet               Do not build dotnet core API and packages."
     echo "    --pack-only                 Only creates packages. (Cannot be combined with --clean)"
@@ -50,8 +50,7 @@ process_args ()
     # separate word. The quotes around `$@' are essential!
     # We need TEMP as the `eval set --' would nuke the return value of getopt.
     TEMP=`getopt -o xo:C:T:cn:j: \
-          -l xtrace,build-root:,config:,clean,toolset:,use-zlog,use-openssl,use-libwebsockets,\
-		  with-memcheck,pack-only,skip-unittests,skip-dotnet,os:,nuget-folder:,jobs: \
+          -l xtrace,build-root:,config:,clean,toolset:,use-zlog,use-openssl,use-libwebsockets,with-memcheck,pack-only,skip-unittests,skip-dotnet,os:,nuget-folder:,jobs: \
          -- "$@"`
 
     if [ $? != 0 ]
@@ -66,56 +65,56 @@ process_args ()
 
     while true
     do
-	case "$1" in
-	    -x | --xtrace)
-		set -x
-                shift ;;
-	    -o | --build-root)
-		build_root="$2"
-                shift 2 ;;
-	    -C | --config)
-                build_configs+=("$2")
-                shift 2 ;;
-	    -T | --toolset)
-		toolset="-T $2"
-                shift 2 ;;
-	    -c | --clean)
-		build_clean=1
-                shift ;;
-	    --with-memcheck)
-		with_memcheck=ON
-                shift ;;
-	    --use-zlog)
-		use_zlog=ON
-                shift ;;
-	    --use-openssl)
-                shift ;;
-	    --use-libwebsockets)
-                shift ;;
-	    --pack-only)
-		build_pack_only=1
-                shift ;;
-	    --skip-unittests)
-		run_unittests=OFF
-                shift ;;
-	    --skip-dotnet)
-		skip_dotnet=1
-                shift ;;
-	    --os)
-		build_os="$2"
-                shift 2 ;;
-	    -n | --nuget-folder)
-		build_nuget_output="$2"
-                shift 2 ;;
-            -j | --jobs)
-                MAKE_PARALLEL_JOBS=$2
-                shift 2 ;;
-	    --) shift
-                break ;; # Terminate the while loop at the last option
-	    *)
-		usage
-                shift ;
-	esac
+    case "$1" in
+        -x | --xtrace)
+            set -x
+            shift ;;
+        -o | --build-root)
+            build_root="$2"
+            shift 2 ;;
+        -C | --config)
+            build_configs+=("$2")
+            shift 2 ;;
+        -T | --toolset)
+            toolset="-T $2"
+            shift 2 ;;
+        -c | --clean)
+            build_clean=1
+            shift ;;
+        --with-memcheck)
+            with_memcheck=ON
+            shift ;;
+        --use-zlog)
+            use_zlog=ON
+            shift ;;
+        --use-openssl)
+            shift ;;
+        --use-libwebsockets)
+            shift ;;
+        --pack-only)
+            build_pack_only=1
+            shift ;;
+        --skip-unittests)
+            run_unittests=OFF
+            shift ;;
+        --skip-dotnet)
+            skip_dotnet=1
+            shift ;;
+        --os)
+            build_os="$2"
+            shift 2 ;;
+        -n | --nuget-folder)
+            build_nuget_output="$2"
+            shift 2 ;;
+        -j | --jobs)
+            MAKE_PARALLEL_JOBS=$2
+            shift 2 ;;
+        --) shift
+            break ;; # Terminate the while loop at the last option
+        *)
+        usage
+            shift ;
+    esac
     done
 }
 
@@ -124,34 +123,34 @@ process_args ()
 # -----------------------------------------------------------------------------
 native_build()
 {
-	if [ $build_pack_only == 0 ]; then
-		echo -e "\033[1mBuilding native...\033[0m"
-		for c in ${build_configs[@]}; do
-			echo -e "\033[1m    ${c}...\033[0m"
-			mkdir -p "${build_root}/cmake/${c}"
-			cd "${build_root}/cmake/${c}" > /dev/null
+    if [ $build_pack_only == 0 ]; then
+        echo -e "\033[1mBuilding native...\033[0m"
+        for c in ${build_configs[@]}; do
+            echo -e "\033[1m    ${c}...\033[0m"
+            mkdir -p "${build_root}/cmake/${c}"
+            cd "${build_root}/cmake/${c}" > /dev/null
 
-			cmake $toolset -DCMAKE_BUILD_TYPE=$c -Drun_unittests:BOOL=$run_unittests \
-			      -Dmem_check:BOOL=$with_memcheck -Duse_zlog:BOOL=$use_zlog "$repo_root" \
+            cmake $toolset -DCMAKE_BUILD_TYPE=$c -Drun_unittests:BOOL=$run_unittests \
+                  -Dmem_check:BOOL=$with_memcheck -Duse_zlog:BOOL=$use_zlog "$repo_root" \
                               -DLWS_IPV6:BOOL=ON || \
-			    return 1
-			# Start as much parallel jobs as requested by the user.
-			# Until the load average equals the number of cores.
-			# Be verbose if something goes wrong
-			make -j$MAKE_PARALLEL_JOBS --load-average=`nproc` || make VERBOSE=1 || \
-				return 1
-			if [ $run_unittests == OFF ]; then
-				echo "Skipping unittests..."
-			else
-				#use doctored (-DPURIFY no-asm) openssl
-				export LD_LIBRARY_PATH=/usr/local/ssl/lib
-				ctest -C "$c" --output-on-failure || \
-					return 1
-				export LD_LIBRARY_PATH=
-			fi
-		done
-	fi
-	return 0
+                return 1
+            # Start as much parallel jobs as requested by the user.
+            # Until the load average equals the number of cores.
+            # Be verbose if something goes wrong
+            make -j$MAKE_PARALLEL_JOBS --load-average=`nproc` || make VERBOSE=1 || \
+                return 1
+            if [ $run_unittests == OFF ]; then
+                echo "Skipping unittests..."
+            else
+                #use doctored (-DPURIFY no-asm) openssl
+                export LD_LIBRARY_PATH=/usr/local/ssl/lib
+                ctest -C "$c" --output-on-failure || \
+                    return 1
+                export LD_LIBRARY_PATH=
+            fi
+        done
+    fi
+    return 0
 }
 
 # -----------------------------------------------------------------------------
@@ -159,44 +158,44 @@ native_build()
 # -----------------------------------------------------------------------------
 managed_build()
 {
-	if [ $skip_dotnet == 1 ]; then
-		echo "Skipping dotnet..."
-	else
-		if dotnet --version; then
-			cd "${repo_root}/api/csharp" > /dev/null
-			if [ $build_pack_only == 0 ]; then
-			    echo -e "\033[1mBuilding dotnet...\033[0m"
-			    dotnet restore || exit 1
-			fi
-			for c in ${build_configs[@]}; do
-				echo -e "\033[1m    ${c}...\033[0m"
+    if [ $skip_dotnet == 1 ]; then
+        echo "Skipping dotnet..."
+    else
+        if dotnet --version; then
+            cd "${repo_root}/api/csharp" > /dev/null
+            if [ $build_pack_only == 0 ]; then
+                echo -e "\033[1mBuilding dotnet...\033[0m"
+                dotnet restore || exit 1
+            fi
+            for c in ${build_configs[@]}; do
+                echo -e "\033[1m    ${c}...\033[0m"
 
-				mkdir -p "${build_nuget_output}/${c}"
-				mkdir -p "${build_root}/${c}"
+                mkdir -p "${build_nuget_output}/${c}"
+                mkdir -p "${build_root}/${c}"
 
-				for f in $(find . -type f -name "*.csproj"); do
-					grep -q netstandard1.3 $f
-					if [ $? -eq 0 ]; then
-						echo -e "\033[1mBuilding ${f} as netstandard1.3\033[0m"
-						dotnet build -c $c -o "${build_root}/${c}" \
-							--framework netstandard1.3 $f \
-							|| return $?
-					else
-						grep -q netcoreapp1.1 $f
-						if [ $? -eq 0 ]; then
-							echo -e "\033[1mBuilding ${f} as netcoreapp1.1\033[0m"
-							dotnet build -c $c -o "${build_root}/${c}" \
-								--framework netcoreapp1.1 $f \
-								|| return $?
-						fi
-					fi
-				done
-			done
-		else
-			echo "No dotnet installed, skipping dotnet build."
-		fi
-	fi
-	return 0
+                for f in $(find . -type f -name "*.csproj"); do
+                    grep -q netstandard1.3 $f
+                    if [ $? -eq 0 ]; then
+                        echo -e "\033[1mBuilding ${f} as netstandard1.3\033[0m"
+                        dotnet build -c $c -o "${build_root}/${c}" \
+                            --framework netstandard1.3 $f \
+                            || return $?
+                    else
+                        grep -q netcoreapp1.1 $f
+                        if [ $? -eq 0 ]; then
+                            echo -e "\033[1mBuilding ${f} as netcoreapp1.1\033[0m"
+                            dotnet build -c $c -o "${build_root}/${c}" \
+                                --framework netcoreapp1.1 $f \
+                                || return $?
+                        fi
+                    fi
+                done
+            done
+        else
+            echo "No dotnet installed, skipping dotnet build."
+        fi
+    fi
+    return 0
 }
 
 # -----------------------------------------------------------------------------
@@ -212,19 +211,19 @@ managed_build()
 # -----------------------------------------------------------------------------
 main()
 {
-	if [ -z "$build_configs" ]; then
-		build_configs=(Debug Release)
-	fi
-	echo "Building ${build_configs[@]}..."
-	if [ $build_clean == 1 ]; then
-		echo "Cleaning previous build output..."
-		rm -r -f "${build_root}"
-	fi
-	mkdir -p "${build_root}"
-	native_build \
-		|| return $?
-	managed_build \
-		|| return $?
+    if [ -z "$build_configs" ]; then
+        build_configs=(Debug Release)
+    fi
+    echo "Building ${build_configs[@]}..."
+    if [ $build_clean == 1 ]; then
+        echo "Cleaning previous build output..."
+        rm -r -f "${build_root}"
+    fi
+    mkdir -p "${build_root}"
+    native_build \
+        || return $?
+    managed_build \
+        || return $?
 }
 
 process_args $*
@@ -235,9 +234,9 @@ main
 echo "$?" 
 cd "$current_root" > /dev/null
 if [ $? -eq 0 ]; then
-	echo "... Success!" 
+    echo "... Success!" 
 else
-	exit $?
+    exit $?
 fi
 
 
